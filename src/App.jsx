64c3c9cc9972ffe7564ff825455d7e979242a80c
Jessica Armstrong <jeyajay1997@gmail.com>
2025-04-10 15:00:57 +0000

    page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Gallery from './pages/Gallery'
+import Contact from './pages/Contact'
 
 function App() {
 
 
   return (
-    <div>Home</div>
+    <div>
+<BrowserRouter>
+<Routes>
+  <Route index element ={<Home/>} />
+  <Route path = '/about' element ={<About />} /> 
+  <Route path = '/contact' element ={<Contact />} /> 
+  <Route path = '/gallery' element ={<Gallery />} /> 
+</Routes>
+</BrowserRouter>
+    </div>
   )
 }
 
